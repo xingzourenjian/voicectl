f432ce2dd033ace2f3748e441975371e033d9e80
行走人间 <wangyi31501@outlook.com>
2024-07-31 20:31:35 +0800

    语音识别

---
+++ 语音识别/gec6818/voicectl.c
@@ -0,0 +1,68 @@
+﻿#include "common.h"
+
+//#define REC_CMD  "./arecord -d4 -c1 -r16000 -traw -fS16_LE cmd.pcm"
+#define REC_CMD  "arecord -d3 -c1 -r16000 -traw -fS16_LE cmd.pcm"
+#define PCM_FILE "./cmd.pcm"
+/* -d：录音时长（duration）
+-c：音轨（channels）
+-r：采样频率（rate）
+-t：封装格式（type）
+-f：量化位数（format） */
+
+void catch (int sig)
+{
+	if (sig == SIGPIPE)
+	{
+		printf("killed by SIGPIPE\n");
+		exit(0);
+	}
+}
+
+int main(int argc, char const* argv[]) // ./voicectl 192.168.31.20
+{
+	// if (argc != 2)
+	// {
+	// 	printf("Usage: %s <ubuntu-IP>\n", argv[0]);
+	// 	exit(0);
+	// }
+	signal(SIGPIPE, catch);
+	// 初始化TCP客户端套接字，用于连接到语音识别服务器(即ubuntu)
+	int sockfd_tcp = init_tcp_socket("192.168.180.1");
+	// 初始化本地UDP套接字
+	int sockfd_udp = init_udp_socket();
+
+	int id_num = -1; // 识别后的指令id
+	while (1)
+	{
+		// 1，调用arecord来录一段音频
+		printf("please to start REC in 3s...\n");
+
+		// 在程序中执行一条命令  “录音的命令”
+		system(REC_CMD);
+
+				
+		// 2，将录制好的PCM音频发送给语音识别引擎
+		send_pcm(sockfd_tcp, PCM_FILE);
+
+		// 3，等待对方回送识别结果（字符串ID）
+		xmlChar* id = wait4id(sockfd_tcp);
+		if (id == NULL)
+		{
+			continue;
+		}
+
+
+		id_num = atoi((char*)id);
+		if (id_num == 999)
+		{
+			printf("bye-bye!\n");
+			exit(1);
+		}
+
+		// udp发送数据给接收端, 接收端收到数据id后，再决定执行什么功能
+		send_id(sockfd_udp, id_num);
+		
+	}
+
+	return 0;
+}
